e60604df3ebf5c7d0ccc87e96d98076df28d9b9a
Said Chama <said.chama@gmail.com>
2026-02-13 16:01:27 -0300

    feat: add contact data translation to english

---
+++ app/(client)/contato/contact.data.ts
@@ -0,0 +1,34 @@
+export const contactStaticData = {
+	label: "Contato",
+	title: "Enviar uma mensagem",
+	description:
+		"Preencha os dados abaixo e entraremos em contato para entender \
+        melhor sua necessidade e avaliar a melhor forma de auxiliá-lo.",
+	nameInput: "Nome completo",
+	nameInputPlaceholder: "Digite seu nome",
+	emailInput: "E-mail",
+	emailInputPlaceholder: "seuemail@exemplo.com",
+	messageInput: "Mensagem",
+	messageInputPlaceholder: "Conte brevemente como podemos ajudar",
+	sendButtonMessage: "Enviar mensagem",
+	messageReason:
+		"As informações enviadas serão utilizadas exclusivamente \
+    para retorno do contato.",
+};
+
+export const contactStaticEnglishData = {
+	label: "Contact",
+	title: "Send a message",
+	description:
+		"Fill in the details below and we will get in touch to better understand \
+        your needs and evaluate the best way to assist you.",
+	nameInput: "Full name",
+	nameInputPlaceholder: "Enter your name",
+	emailInput: "E-mail",
+	emailInputPlaceholder: "youremail@example.com",
+	messageInput: "Message",
+	messageInputPlaceholder: "Briefly tell us how we can help",
+	sendButtonMessage: "Send message",
+	messageReason:
+		"The information sent will be used exclusively for contact return.",
+};
