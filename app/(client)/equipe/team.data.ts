3b1db11d25ee7af2121cf83c69d1b7f56463f547
Said Chama <said.chama@gmail.com>
2026-02-13 15:53:44 -0300

    feat: add team data translation to english

---
+++ app/(client)/equipe/team.data.ts
@@ -0,0 +1,39 @@
+export const teamStaticData = {
+	label: "Conheça o time",
+	title: "Profissionais Qualificados",
+	description:
+		"Contamos com uma equipe de profissionais dedicados e experientes, comprometidos em oferecer atendimento jurídico estratégico, ético e personalizado para cada cliente.",
+	team: "Equipe",
+	loadingTeam: "Carregando equipe...",
+	viewProfile: "Ver Currículo",
+	close: "Fechar",
+	bio: "Biografia",
+	oab: "OAB",
+	email: "Email",
+	education: "Formação",
+	languages: "Idiomas",
+	lattes: "Currículo Lattes",
+	openLattes: "Abrir Currículo",
+	instagram: "Instagram",
+	linkedin: "LinkedIn",
+};
+
+export const teamStaticEnglishData = {
+	label: "Meet our team",
+	title: "Qualified Professionals",
+	description:
+		"We have a team of dedicated and experienced professionals committed to providing strategic, ethical, and personalized legal services for each client.",
+	team: "Team",
+	loadingTeam: "Loading team...",
+	viewProfile: "See Resume",
+	close: "Close",
+	bio: "Biography",
+	oab: "OAB",
+	email: "Email",
+	education: "Education",
+	languages: "Languages",
+	lattes: "Lattes Resume",
+	openLattes: "Open Resume",
+	instagram: "Instagram",
+	linkedin: "LinkedIn",
+};
