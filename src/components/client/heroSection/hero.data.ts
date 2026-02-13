ba8abb868ee92a5a449a4aac0baad0b9eeee141f
Said Chama <said.chama@gmail.com>
2026-02-13 11:46:22 -0300

    feat: add hero data translation to english

--- src/components/client/heroSection/hero.data.ts
+++ src/components/client/heroSection/hero.data.ts
@@ -10,3 +10,16 @@ export const heroData = {
 		return this.title.length * this.typingSpeed + this.delayBetweenTexts;
 	},
 } as const;
+
+export const heroEnglishData = {
+	title: "Juridic Excellence to Guide Your Decisions",
+	subtitle:
+		"Complete Juridic Assistance with a Focus on Security and Transparency.",
+	typingSpeed: 50,
+	subtitleSpeed: 30,
+	delayBetweenTexts: 300,
+
+	get totalDelay() {
+		return this.title.length * this.typingSpeed + this.delayBetweenTexts;
+	},
+} as const;
